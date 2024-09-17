cec17d2eb592537f4fca18dc7f589b84b9b9ca6f
Arie Setiadi <arie@eannovate.com>
2024-09-17 14:30:38 +0800

    use light mode as default theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
+
+  app: {
+    head: {
+      bodyAttrs: { "data-theme": "light" },
+    },
+  },
 });
